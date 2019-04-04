f8fd1501522239a943a2e120f967d57270e94d59
cerise <cerise.andres@yahoo.fr>
2019-04-04 17:02:09 +0200

    mise en place de l'image

--- resources/js/components/ModifCharac.vue
+++ resources/js/components/ModifCharac.vue
@@ -1,18 +1,40 @@
 
 <template>
+  <div>
   <form>
     <div class="form-group">
-      <label for="formGroupExampleInput">HOUSE</label>
+
+      <label>Upload Image</label>
+      <div class="input-group">
+          <span class="input-group-btn">
+              <span class="btn btn-default btn-file">
+                  Browse… <input type="file" id="imgInp" v-on:change="onImageChange">
+              </span>
+          </span>
+          <input type="text" class="form-control" v-model="nameImg">
+      </div>
+      <img id='img-upload'/>
+
+      <label for="formInput">HOUSE</label>
       <input type="text" class="form-control" v-model="house" name="house">
-      <label for="exampleFormControlTextarea1">DESCRIPTION</label>
+      <label for="formControlTextarea">DESCRIPTION</label>
       <textarea class="form-control" v-model="description" name="description" rows="3"></textarea>
       <a v-on:click="modify" class="btn btn-lg btn-block">accept</a>
     </div>
   </form>
+<form enctype="multipart/form-data"  @submit="test" id="fupForm" >
+  <div class="form-group">
+        <label for="file">File</label>
+        <input type="file" class="form-control" id="file" v-on:change="onImageChange" name="file" required />
+        <input type="submit" name="submit"  class="btn btn-danger submitBtn" value="SAVE"/>
+    </div>
+</form>
+</div>
 </template>
 
 <script>
 import $ from 'jquery'
+import axios from 'axios'
 export default {
   name: 'ModifCharac',
   data: function() {
@@ -20,12 +42,16 @@ export default {
       description:"",
       house: "",
       tab:[],
-      api:""
+      api:"",
+      image:"",
+      nameImg:"",
+      filee:"",
     }
   },
   methods: {
       modify: function() {
         var self=this;
+
         var urlapi= "http://localhost/aionGRP/api.php?w=api&name="+name+"&password="+password;
         $.getJSON(urlapi, function(data){
           self.api=data;
@@ -46,6 +72,27 @@ export default {
           self.$router.push({name: 'mycharacters'});
         });
       },
+      test: function (e) {
+        var self=this;
+        console.log('etape1');
+         e.preventDefault();
+         let formData = new FormData();
+         formData.append('image', self.image);
+         const config = {
+
+    headers: { 'content-type': 'multipart/form-data' }
+
+}
+
+         axios.post('http://localhost/aionGRP/test.php', formData, config)
+         .then(function (response) {
+               console.log('marche'+response.data.success);
+             })
+            .catch(function (error) {
+                   console.log('erreur'+error);
+                 });
+
+      },
       show: function(){
         var self=this;
         var urlapi= "http://localhost/aionGRP/api.php?w=api&name="+name+"&password="+password;
@@ -60,7 +107,11 @@ export default {
           });
 
         });
-      }
+      },
+      onImageChange: function(e){
+        console.log(e.target.files[0]);
+        this.image = e.target.files[0];
+      },
     },
    mounted() {
      this.show()
@@ -71,5 +122,28 @@ export default {
 body{
     overflow-wrap: break-word;
   }
+.btn-file {
+    position: relative;
+    overflow: hidden;
+}
+.btn-file input[type=file] {
+    position: absolute;
+    top: 0;
+    right: 0;
+    min-width: 100%;
+    min-height: 100%;
+    font-size: 100px;
+    text-align: right;
+    filter: alpha(opacity=0);
+    opacity: 0;
+    outline: none;
+    background: white;
+    cursor: inherit;
+    display: block;
+}
+
+#img-upload{
+    width: 100%;
+}
 </style>
 </script>
